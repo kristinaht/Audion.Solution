9af4c894165277ce711c5b1f415fd6a2a53673a0
acolistro <arcolistro@gmail.com>
2020-04-06 14:51:57 -0700

    add put and delete methods, tested all methods successfully with Postman.

--- AudionApi/Controllers/QuestionsController.cs
+++ AudionApi/Controllers/QuestionsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AudionApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AudionApi.Controllers
 {
@@ -25,15 +26,33 @@ namespace AudionApi.Controllers
 
     [HttpGet("{id}")]
     public ActionResult<Question> Get(int id)
-{
-    return _db.Questions.FirstOrDefault(entry => entry.QuestionId == id);
-}
+    {
+      return _db.Questions.FirstOrDefault(entry => entry.QuestionId == id);
+    }
 
     // POST api/questions
     [HttpPost]
     public void Post([FromBody] Question question)
     {
       _db.Questions.Add(question);
+      _db.SaveChanges();
+    }
+
+        // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Question question)
+    {
+      question.QuestionId = id;
+      _db.Entry(question).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/questions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var questionToDelete = _db.Questions.FirstOrDefault(entry => entry.QuestionId == id);
+      _db.Questions.Remove(questionToDelete);
       _db.SaveChanges();
     }
   }
